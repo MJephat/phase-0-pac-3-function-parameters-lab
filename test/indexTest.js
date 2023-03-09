ba4b19519a8fb73c6f7cc876d1dbcb031bfc4608
MJephat <mukundijephat22@gmail.com>
2023-03-09 08:03:57 +0300

    assignment complete

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,7 @@
 require ( './root.js' );
 
 
-describe('introduction(name)', function() {
+describe('introduction(name)', function(){
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
     expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
     expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
