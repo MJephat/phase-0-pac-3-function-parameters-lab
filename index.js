149933b6aeb05fd3aa222edfa60b65eb2aaf0c79
MJephat <mukundijephat22@gmail.com>
2023-03-09 11:06:43 +0300

    "rectfied fail 1"

--- index.js
+++ index.js
@@ -3,26 +3,17 @@ function introduction(name){
 }
 introduction("Aki")
 
-  
-function introductionWithLanguage(name,Language){
-   return `Hi, my name is ${name} and I am learning to program in ${Language}.`
+
+function introductionWithLanguage(name,language){
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
-  introductionWithLanguage("Aki", "Ember.js");
-  introductionWithLanguage("Samip", "React");
-
-//function introductionWithLanguageOptional(name,language=JavaScript){
-  //  return `Hi, my name is ${name}and i am learning to program in ${language}.`
-//}
-//introductionWithLanguageOptional("Gracie","Python");
-
-function introductionWithLanguageOptional(name,JavaScript){
-    return `Hi, my name is Gracie and i am learning to program in javaScript.`
-    }
-    introductionWithLanguageOptional("Gracie","Python");
-
-
+  
+ 
 function introductionWithLanguageOptional(name, language="JavaScript"){
-    return `Hi, my name is Gracie and I am learning to program in Python.`
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    }
+    
+function introductionWithLanguageOptional(name, language="JavaScript"){
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-introductionWithLanguageOptional("Gracie", "Python");
 
